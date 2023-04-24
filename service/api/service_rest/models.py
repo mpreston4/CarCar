8c8fdeaa48fd4c8475dffab494245abe188d78db
Nesh Sharma <jaypra03@gmail.com>
2023-04-24 14:26:13 -0700

    created models for the service and created a technician view/ endpoint

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Technicians (models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.first_name
+    
+class AutomobileVO (models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    import_href = models.CharField(max_length=200, unique=True)
+
+class Appointment (models.Model):
+    date_time = models.DateTimeField()
+    reason = models.TextField()
+    status = models.CharField(max_length=200)
+    vin = models.ForeignKey(
+        AutomobileVO,
+        related_name= "appointment_vin",
+        on_delete=models.CASCADE,
+    )
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Technicians,
+        related_name="appointment_technician",
+        on_delete=models.CASCADE,        
+    )
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import Technicians
+from django.http import JsonResponse
 
-# Create your views here.
+class TechnicianEncoder(ModelEncoder):
+    model = Technicians
+    properties = [
+        "first_name", 
+        "last_name",
+        "employee_id",
+    ]
+
+@require_http_methods(["GET", "POST", "DELETE",])
+def api_technicians(request):
+    if request.method == "GET":
+        technicians = Technicians.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianEncoder,
+        )
+    elif request.method == "POST":
+        content = json.loads(request.body)
+        try:
+            

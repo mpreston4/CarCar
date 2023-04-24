b5c84985e130b19aa10240a3acc115d89d08e984
mpreston4 <mick469@gmail.com>
2023-04-24 17:23:41 -0400

    models added for sales

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    href = models.CharField(max_length=100)
+    sold = models.BooleanField(default=False)
+
+
 # Create your models here.
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Sale(models.Model):
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales_automoble",
+        on_delete=models.CASCADE,
+    )
+    salesperson = models.ForeignKey(
+        Salesperson, related_name="sales_sales_person", on_delete=models.CASCADE
+    )
+    customer = models.ForeignKey(
+        Customer, related_name="sales_customer", on_delete=models.CASCADE
+    )
+    price = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.automobile
